88102292e271322897b4567278152e051e217b1d
Davina Arafa <72923009+dappiess@users.noreply.github.com>
2022-04-04 11:43:32 +0700

    commit on master

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+      <h1> Hello Dapp! </h1>
+      <nuxt-link to="/home">Home</nuxt-link>
+  </div>
+</template>
+
+<style>
+  body {
+    font-family: sans-serif;
+    margin: 5%
+  }
+</style>
